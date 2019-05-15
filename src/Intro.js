db2db4c69d3da18962995e664e88a0a05f3006af
Alan Triacca <atriacca@yahoo.com>
2019-05-14 22:04:55 -0600

    minor responsive fix

--- src/Intro.js
+++ src/Intro.js
@@ -5,7 +5,6 @@ function Intro() {
         <div>
             <div className='name'>
             <img src={require('./atriacca-p1.jpeg')} alt='Unable to render.' />
-            <br></br>
             <h2>ALAN</h2>
             <h3>TRIACCA</h3>
             <h5>Full Stack JavaScript Developer</h5>
