db5b79e3d5903369d9a051fb81d753a71d7ae13a
Alan Triacca <atriacca@yahoo.com>
2019-09-27 18:32:03 -0600

    update resume

--- src/Resume.js
+++ src/Resume.js
@@ -5,15 +5,15 @@ function Resume() {
         <div className='btnwrapper'>
             <h4>Resume</h4>
             <span>
-                <a href={require('./resources/atriaccaresume20190819.docx')} download>
+                <a href={require('./resources/atriaccaresume20190927.docx')} download>
                 <button className='btn'>Download .doc file</button></a>
             </span>
             <span>
-                <a href={require('./resources/atriaccaresume20190805.jpg')} download>
+                <a href={require('./resources/atriaccaresume20190927.jpg')} download>
                 <button className='btn'>Download .jpg file</button></a>
             </span>
             <span>
-                <a href={require('./resources/atriaccaresume20190805.odt')} download>
+                <a href={require('./resources/atriaccaresume20190927.odt')} download>
                 <button className='btn'>Download OpenDoc file</button></a>
             </span>
             {/* <span>
