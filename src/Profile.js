ae7c2dfaaa91d49ed9d815f02c7708823cde6f3f
Alan Triacca <atriacca@yahoo.com>
2019-09-29 02:10:31 -0600

    update portfolio profile

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ function Profile() {
             <p>Hi! My name is Alan Triacca and I am a full stack software developer based in Utah.</p>
             <p>I'm just as passionate about making things as I am with improving everything I see and just finding ways to make the planet a better place for everyone.</p>
             <p>After graduating from the University of Connecticut, I spent more than ten years with two different companies. First in the semiconductor industry, mostly as an Applications Engineer and Account Manager. Then in investor education, as a Client Service/Technical Support specialist.</p>
-            <p>I completed the Full Stack JavaScript Web Development Bootcamp at V School in Salt Lake City on May 17, 2019. I'm very excited to embark on my new career!</p>
+            <p>I completed the Full Stack JavaScript Web Development Bootcamp at V School in Salt Lake City on May 17, 2019. I was a Teaching Assistant at V School from May to September 2019. I'm very excited to embark on my new career!</p>
         </div>
     )
 }
